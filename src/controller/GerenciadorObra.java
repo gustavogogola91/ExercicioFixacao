60efdbce1a6d49f970499b339e4dbda6cf2c2377
GustavoGogola91 <90578528+GustavoGogola91@users.noreply.github.com>
2024-05-21 20:53:09 -0300

    metodo para excluir adicionado

--- src/controller/GerenciadorObra.java
+++ src/controller/GerenciadorObra.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import model.Obra;
 
 public class GerenciadorObra {
-    
+
     private static ArrayList<Obra> lista = new ArrayList<Obra>();
 
     public static void adicionarObra(Obra obra) {
@@ -17,11 +17,25 @@ public class GerenciadorObra {
     public static Obra buscarObra(String titulo) throws Exception {
 
         for (Obra obra : lista) {
-            if(obra.getTitulo().equals(titulo)) {
+            if (obra.getTitulo().equals(titulo)) {
                 return obra;
             }
         }
 
         throw new Exception("Não há obras com este nome");
     }
+
+    public static void excluirObra(String titulo) throws Exception {
+
+        Obra obra = buscarObra(titulo);
+
+        lista.remove(obra);
+    }
+
+    public static ArrayList<Obra> listarTodas() {
+
+        return lista;
+    }
+
+
 }
