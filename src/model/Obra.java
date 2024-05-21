244fc700493da0fab5757541bc22c8016870358e
GustavoGogola91 <90578528+GustavoGogola91@users.noreply.github.com>
2024-05-21 20:32:59 -0300

    Classe base adicionada

---
+++ src/model/Obra.java
@@ -0,0 +1,60 @@
+package model;
+
+public class Obra {
+
+    protected String titulo;
+    protected String artista;
+    protected int anoCriacao;
+    protected String tipo; // TODO: alterar para herança
+    protected String localizacao;
+
+    
+
+    public Obra(String titulo, String artista, int anoCriacao, String localizacao) {
+        this.titulo = titulo;
+        this.artista = artista;
+        this.anoCriacao = anoCriacao;
+        this.localizacao = localizacao;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+
+    public int getAnoCriacao() {
+        return anoCriacao;
+    }
+
+    public void setAnoCriacao(int anoCriacao) {
+        this.anoCriacao = anoCriacao;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getLocalizacao() {
+        return localizacao;
+    }
+
+    public void setLocalizacao(String localizacao) {
+        this.localizacao = localizacao;
+    }
+
+}
